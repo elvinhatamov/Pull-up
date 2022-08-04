396b41c0d45213d4669469ac96bdb65d8c1f437b
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-04 16:12:29 -0400

    added nav and logo component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./Components/Logo/Logo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PULL UP APP HOME PAGE</h1>
+      <nav>
+        Nav Bar
+        <Logo />
+      </nav>
     </div>
   );
 }
---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo(props) {
+  return (
+    <div className="Logo">
+      <h1>Logo Goes Here</h1>
+    </div>
+  );
+}
+
+export default Logo;
