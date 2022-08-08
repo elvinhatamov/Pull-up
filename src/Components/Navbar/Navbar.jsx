56c7b2a680f2d19e9148ca9fbb1c22cf1a068146
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-07 20:51:41 -0400

    added basic navbar with no anchor tags

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar(props) {
+  return (
+    <nav className="Navbar">
+      <div className="pullup-logo-div">
+        <h1>
+          <span className="pullup-logo-p">P</span>ULLUP
+        </h1>
+      </div>
+      <div className="nav-buttons-div">
+        <h1>empty atm but will have buttons</h1>
+      </div>
+      <div className="signup-login-div">
+        <img src={process.env.PUBLIC_URL + "images/usericon.png"} />
+        <h1>Login | Signup</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
