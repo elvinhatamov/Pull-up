b04c460c909519c06a392c0e4436a1ae524b62df
Shadab K <skarim@Shadabs-MacBook-Air.local>
2022-08-11 00:23:00 -0400

    models initialized

---
+++ models/reservation.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema(
+  {
+    customer: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    listing: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    car: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    totalCost: {
+      type: Float,
+      required: true,
+    },
+    dateStart: {
+      type: Date,
+      required: true,
+    },
+    dateEnd: {
+      type: Date,
+      required: type,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Reservation", reservationSchema);
