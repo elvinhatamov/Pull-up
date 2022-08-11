497b1d83ceba15c2e43c8ed89e74eaab965066e0
Shadab K <skarim@Shadabs-MacBook-Air.local>
2022-08-11 00:56:39 -0400

    model changes #1

--- models/reservation.js
+++ models/reservation.js
@@ -1,36 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reservationSchema = new Schema(
-  {
-    customer: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    listing: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    car: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
-    totalCost: {
-      type: Float,
-      required: true,
-    },
-    dateStart: {
-      type: Date,
-      required: true,
-    },
-    dateEnd: {
-      type: Date,
-      required: type,
-    },
+const reservationSchema = new Schema({
+  customer: {
+    type: Schema.Types.ObjectId,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  listing: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  car: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  totalCost: {
+    type: Float,
+    required: true,
+  },
+  dateStart: {
+    type: Date,
+    required: true,
+  },
+  dateEnd: {
+    type: Date,
+    required: type,
+  },
+});
 
 module.exports = mongoose.model("Reservation", reservationSchema);
