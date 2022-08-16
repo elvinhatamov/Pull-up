7f37a4af4b1ac6b66d692e04dece85d41f94c624
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-16 18:57:08 -0400

    fix create form and make it add a listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,25 +1,22 @@
 //model here
-const Listings = require('../models/listing')
+const Listings = require("../models/listing");
 
-async function deleted(req,res){
+async function deleted(req, res) {
   try {
-    
     await Listings.findByIdAndDelete(req.params.id);
-    res.status(200).json("Ithem has been deleted")
+    res.status(200).json("Ithem has been deleted");
   } catch (error) {
-    res.status(500).json(error)
-    
+    res.status(500).json(error);
   }
 }
 
-
-
 //MAKE THIS ASYNC LATER WHEN WE ADD ACTUAL QUERIES
 async function create(req, res) {
- console.log(req.body)
- let list = new Listings(req.body)
+  console.log("Ajax works for hosting form to create!");
+  console.log(req.body);
+  let list = new Listings(req.body);
   try {
-     await list.save()
+    await list.save();
     //some query to add listing into the database
     console.log("pathing works on api/listings/create!!");
     res.status(200).json({ text: "Adding a Listing to MongoDB in the future" });
@@ -30,25 +27,23 @@ async function create(req, res) {
 
 async function update(req, res) {
   try {
-    let updateLot = await Listings.findByIdAndUpdate(req.params.id,{ $set: req.body})
-    res.status(200).json(updateLot)
+    let updateLot = await Listings.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json(updateLot);
   } catch (error) {
     return res.status(500).json(error);
   }
 }
 
-async function list(req,res){
+async function list(req, res) {
   try {
-    let list = await Listings.find().select(req.body.address)
-    res.status(200).json(list)
-
+    let list = await Listings.find().select(req.body.address);
+    res.status(200).json(list);
   } catch (error) {
-     res.status(500).json(error)
-    
+    res.status(500).json(error);
   }
 }
-
- 
 
 //MAKE THIS ASYNC LATER WHEN WE ADD ACTUAL QUERIES
 async function index(req, res) {
@@ -68,5 +63,5 @@ module.exports = {
   index,
   update,
   deleted,
-  list
+  list,
 };
