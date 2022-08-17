4e257765375c2d5a992edc5625ebce34209ae272
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-17 17:09:40 -0400

    bad date selection logic

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -12,6 +12,8 @@ async function create(req, res) {
     const date1 = new Date(req.body.dateFrom);
     const date2 = new Date(req.body.dateTo);
     console.log(`Date From is ${date1} and Date To is ${date2}`);
+    //grab today's date too
+    const today = new Date();
 
     //one day in milliseconds
     const one_day = 1000 * 60 * 60 * 24;
@@ -22,15 +24,23 @@ async function create(req, res) {
 
     console.log("Days you attempted to book for: ", totalDays);
 
+    //check if any field is in the past
+    const day1past = Math.round((date1-today) / one_day);
+    const day2past = Math.round((date2-today) / one_day);
+
+
     //Error handling for invalid date ranges
     if (totalDays < 0) {
       return res.status(200).json({
         userError: true,
         msg: "Your check-in and check-out dates are backwards!",
       });
-    } else {
+    } else if ()
+    
+    {
       res.status(200).json("Adding a Reservation to MongoDB in the future");
     }
+    
     //PUSH DATES INTO DAYSBOOKED IN LISTING
     //   const dateArray = [];
     // let currentDate = new Date(startDate);
