65eef356a911d6af6756064764919a5c4de6034b
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-17 22:36:20 -0400

    changed loading script method

--- src/Pages/MapListingsPage/MapListingsPage.jsx
+++ src/Pages/MapListingsPage/MapListingsPage.jsx
@@ -1,6 +1,13 @@
 import Map from "../../Components/Map/Map";
+import { useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function MapListingsPage() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <div style={{ width: "100vw", height: "90vh" }}>
       <Map />
